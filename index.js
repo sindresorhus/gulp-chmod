c9f853c91c7ad8328e2d5f13cf7ea1682d7e1bf5
jbsulli <jb_sulli@yahoo.com>
2016-09-08 16:39:05 -0700

    Allow seperate dirMode values

--- index.js
+++ index.js
@@ -27,12 +27,34 @@ function normalize(mode) {
 }
 
 module.exports = function (mode, dirMode) {
-	if (typeof mode !== 'number' && typeof mode !== 'object') {
-		throw new TypeError('Expected a number or object');
+	// check mode
+	if (mode !== undefined && typeof mode !== 'number' && typeof mode !== 'object') {
+		throw new TypeError('Expected mode to be null/undefined, a number, or an object');
+	}
+	
+	// if dirMode === true, use mode argument for directories as well
+	if(dirMode === true){
+		dirMode = mode;
+	}
+	
+	// check dirMode
+	if (dirMode !== undefined && typeof dirMode !== 'number' && typeof dirMode !== 'object'){
+		throw new TypeError('Expected dirMode to be null/undefined, true, a number, or an object');
 	}
 
 	return through.obj(function (file, enc, cb) {
-		if (file.isNull() && (dirMode !== true || (!file.stat || !file.stat.isDirectory || !file.stat.isDirectory()))) {
+		// default to file mode
+		var curMode = mode;
+		
+		// file is a directory?
+		if (file.isNull() && file.stat && file.stat.isDirectory && file.stat.isDirectory()) {
+			// use dirMode because this is a directory
+			curMode = dirMode;
+		}
+		
+		// is no mode defined?
+		if (curMode === undefined || curMode === null){
+			// pass it through
 			cb(null, file);
 			return;
 		}
@@ -40,12 +62,12 @@ module.exports = function (mode, dirMode) {
 		file.stat = file.stat || {};
 		file.stat.mode = file.stat.mode || defaultMode;
 
-		if (typeof mode === 'object') {
+		if (typeof curMode === 'object') {
 			var statMode = new Mode(file.stat);
-			deepAssign(statMode, normalize(mode));
+			deepAssign(statMode, normalize(curMode));
 			file.stat.mode = statMode.stat.mode;
 		} else {
-			file.stat.mode = parseInt(mode, 8);
+			file.stat.mode = parseInt(curMode, 8);
 		}
 
 		cb(null, file);
