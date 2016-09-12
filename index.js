2dbaa9c481263d859bdfb6ecaef6c0885c96d881
jbsulli <jb_sulli@yahoo.com>
2016-09-12 10:33:02 -0700

    Remove comments and unnecessary checks

--- index.js
+++ index.js
@@ -27,34 +27,26 @@ function normalize(mode) {
 }
 
 module.exports = function (mode, dirMode) {
-	// check mode
 	if (mode !== undefined && typeof mode !== 'number' && typeof mode !== 'object') {
 		throw new TypeError('Expected mode to be null/undefined, a number, or an object');
 	}
 	
-	// if dirMode === true, use mode argument for directories as well
 	if(dirMode === true){
 		dirMode = mode;
 	}
 	
-	// check dirMode
 	if (dirMode !== undefined && typeof dirMode !== 'number' && typeof dirMode !== 'object'){
 		throw new TypeError('Expected dirMode to be null/undefined, true, a number, or an object');
 	}
 
 	return through.obj(function (file, enc, cb) {
-		// default to file mode
 		var curMode = mode;
 		
-		// file is a directory?
-		if (file.isNull() && file.stat && file.stat.isDirectory && file.stat.isDirectory()) {
-			// use dirMode because this is a directory
+		if (file.isNull() && file.stat.isDirectory()) {
 			curMode = dirMode;
 		}
 		
-		// is no mode defined?
 		if (curMode === undefined || curMode === null){
-			// pass it through
 			cb(null, file);
 			return;
 		}
